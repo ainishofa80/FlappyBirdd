a62abc93b934faa56464696b7d60ede6bd7e1925
ainishofa80 <161667682+ainishofa80@users.noreply.github.com>
2024-03-01 20:24:00 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709299297,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bcronfleppybird-sheet0.png",
+		"images/fleppy_birdremovebgpreview-sheet0.png",
+		"images/pipe_fleppy_birdremovebgpreview-sheet0.png",
+		"images/pipe_fleppy_birdremovebgpreview2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
